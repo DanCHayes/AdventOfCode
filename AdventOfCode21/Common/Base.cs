851f1e8d0537891015ac51343ddf3c5082439298
dan.hayes <dan.hayes@DESKTOP-5H76RTT>
2021-12-03 23:58:43 +0000

    Day 1

---
+++ AdventOfCode21/Common/Base.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace AdventOfCode21.Common
+{
+    public abstract class Base
+    {
+        public static IEnumerable<string> LoadInput(string folder)
+        {
+            var basePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            return File.ReadAllLines($"{basePath}/../../../{folder}/input.txt");
+        }
+    }
+}
---
+++ AdventOfCode21/Day1/Program.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode21.Common;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode21.Day1
+{
+    public class Program : Base
+    {
+        static void Main(string[] args)
+        {
+            var input = LoadInput("Day1")
+                .Select(x => Convert.ToInt32(x));
+
+            Console.WriteLine($"Task1 answer: {Task1(input)}");
+            Console.WriteLine($"Task1 answer: {Task2(input)}");
+        }
+
+        public static int Task1(IEnumerable<int> input)
+        {
+            var counter = 0;
+            var prev = int.MaxValue;            
+            foreach (int number in input)
+            {
+                if (number > prev) counter++;
+                prev = number;
+            }
+
+            return counter;
+        }
+
+        public static int Task2(IEnumerable<int> input)
+        {
+            var windowedInput = input
+                .Window(3)
+                .Select(item => item.Sum());
+
+            return Task1(windowedInput);
+        }
+    }
+}
